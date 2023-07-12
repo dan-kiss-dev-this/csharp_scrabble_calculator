847532a53357628b022ddc07cd8327206938710c
“Daniel <flowfast47@gmail.com>
2023-07-12 11:44:22 -0700

    Create and pass test to see if Dictionary field can be accessed Co-authored By: Jason Church elijahchurch@gmail.com

--- ScrabbleTests/ModelTests/ScrabbleCalculator.cs
+++ ScrabbleTests/ModelTests/ScrabbleCalculator.cs
@@ -28,6 +28,15 @@ namespace Scrabble.Tests
             string word = newScrabbleGame.UserWord;
             Assert.AreEqual(userInput, word);
         }
+
+        [TestMethod]
+        public void GetScrabbleKey_AccessScrabbleKeyField_Dictionary()
+        {
+            Dictionary<char, int> expectedDictionary = new Dictionary<char, int>() { {'a', 1}, {'b', 3}, {'c', 3}, {'d', 2}, {'e', 1}, {'f', 4}, {'g', 2}, {'h', 4}, {'i', 1},  {'j', 8}, {'k', 5}, {'l', 1}, {'m', 3}, {'n', 1}, {'o', 1}, {'p', 3}, {'q', 10}, {'r', 1}, {'s', 1}, {'t', 1}, {'u', 1}, {'v', 4}, {'w', 4}, {'x', 8}, {'y', 4}, {'z', 10} };
+            ScrabbleGame testScrabbleGame = new ScrabbleGame("dog");
+            CollectionAssert.AreEqual(expectedDictionary, testScrabbleGame.ScrabbleKey );
+
+        }
         
     }
 }
