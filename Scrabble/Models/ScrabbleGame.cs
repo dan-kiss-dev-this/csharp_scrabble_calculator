2f2a92202957644545b0e0068730d7880cee7058
“Daniel <flowfast47@gmail.com>
2023-07-12 15:33:08 -0700

    Create GameCollection list field and passed test for it  Co-authored-by: Jason Church <elijahchurch@gmail.com>

--- Scrabble/Models/ScrabbleGame.cs
+++ Scrabble/Models/ScrabbleGame.cs
@@ -9,12 +9,15 @@ namespace Scrabble.Models
         // Business Logic goes here.
         public Dictionary<char, int> ScrabbleKey { get; } = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 } };
 
+        public static List<ScrabbleGame> GameCollection { get; set;} = new List<ScrabbleGame> {};
+
         public int WordScore { get; set; }
 
         public ScrabbleGame(string userWord)
         {
             UserWord = userWord.ToLower();
             CalculateWordScore();
+            GameCollection.Add(this);
         }
 
         private void CalculateWordScore()
